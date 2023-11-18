9bd90c2637893909f5344433ae69d8bb5856b1e3
Swami Venkataramani <swami108@gmail.com>
2023-11-17 22:40:43 -0800

    added photo

--- src/components/photo/Photo.js
+++ src/components/photo/Photo.js
@@ -3,7 +3,7 @@ import React from "react";
 function Photo({ src, alt }) {
   return (
     <div className="flex justify-center items-center">
-      <img src={src} alt={alt} className="max-w-full h-auto border border-gray-300 rounded" />
+      <img src={src} alt={alt} className="max-w-full h-auto border border-gray-300 shadow" />
     </div>
   );
 }
