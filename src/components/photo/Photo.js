2868d434233dbf01b28097a2878b78819cb1eb58
Swami Venkataramani <swami108@gmail.com>
2023-11-22 19:51:36 -0800

    bg change

--- src/components/photo/Photo.js
+++ src/components/photo/Photo.js
@@ -4,7 +4,7 @@ function Photo({ src, alt }) {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <div className={`flex justify-center items-center bg-gray-200 ${!isLoaded ? "animate-pulse" : ""}`}>
+    <div className={`flex justify-center items-center bg-gray-300 ${!isLoaded ? "animate-pulse" : ""}`}>
       <img src={src} alt={alt} loading="lazy" className={`transition-opacity duration-500 ease-in-out max-w-full h-auto border border-gray-300 shadow-md mb-4 md:mb-10 ${isLoaded ? "opacity-100" : "opacity-0"}`} onLoad={() => setIsLoaded(true)} />
     </div>
   );
