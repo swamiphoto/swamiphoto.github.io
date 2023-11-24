b3341d23834f83c14709a5eb0286df8c226e7398
Swami Venkataramani <swami108@gmail.com>
2023-11-23 16:15:59 -0800

    cors

--- api/resize-image.js
+++ api/resize-image.js
@@ -2,6 +2,17 @@ const sharp = require("sharp");
 const fetch = require("node-fetch"); // You'll need to install node-fetch if you don't already have it
 
 module.exports = async (req, res) => {
+  // Add CORS headers
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Adjust this in production
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Access-Control-Allow-Headers, Content-Type, Authorization, Origin, Accept");
+
+  // OPTIONS request handling
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
     // Assuming the request contains a JSON body with an image URL
     const { imageUrl } = req.body;
