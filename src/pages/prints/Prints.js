0e6e9f5c9f2ac85de24ccfe95a75144056a3c5ac
Swami Venkataramani <swami108@gmail.com>
2023-12-03 08:59:46 -0800

    changed url

--- src/pages/prints/Prints.js
+++ src/pages/prints/Prints.js
@@ -20,7 +20,7 @@ const Prints = () => {
         title="San Francisco"
         caption="Seeing the world's most famous bridge glowing in the fog at sunrise was a special experience. This photo was selected as Editor's Favorite by National Geographic, and was also showcased in the Escaype art gallery in San Francisco (2019)."
         alt="Golden Gate Bridge in fog"
-        url="https://swamiphoto.pixieset.com/landscapes/"
+        url="https://store.swamiphoto.com/wallprints/"
       />
       {/* <Photo src={IMAGES.landscapes.paris} layout="print" title="Paris" caption="Katrina Kaif who came to san jose for a nice concert." alt="" /> */}
       <Photo
@@ -29,7 +29,7 @@ const Prints = () => {
         title="Mumbai"
         caption="The Gateway of India, one of India's most iconic monuments, is a historical symbol that represents both the colonization as well as the independence of India. My image was the official poster and backdrop at the Social Media Week conference in Mumbai (2016)."
         alt="Gateway of India"
-        url="https://swamiphoto.pixieset.com/landscapes/"
+        url="https://store.swamiphoto.com/wallprints/"
       />
 
       <Photo
@@ -38,7 +38,7 @@ const Prints = () => {
         title="Sierra"
         caption="A beautiful lake nestled in the Eastern Sierras, this is my attempt at recreating the scene featured in one of macOS's wallpaper."
         alt="A beautiful lake nestled in the Eastern Sierras"
-        url="https://swamiphoto.pixieset.com/landscapes/"
+        url="https://store.swamiphoto.com/wallprints/"
       />
       <Photo
         src={IMAGES.landscapes.pastel}
@@ -46,7 +46,7 @@ const Prints = () => {
         title="Pastel Storm"
         caption="While driving along the Eastern Sierra highway, I noticed a colorful storm brewing in the distance. Where I was, it was raining cats and dogs. I quickly pulled over to the shoulder, managed to take this shot, and rushed back to my car."
         alt="A colorful storm brewing in the distance"
-        url="https://swamiphoto.pixieset.com/landscapes/"
+        url="https://store.swamiphoto.com/wallprints/"
       />
       <Photo
         src={IMAGES.landscapes.kerala}
@@ -55,7 +55,7 @@ const Prints = () => {
         title="Kerala"
         caption="Captured on a tranquil evening in Kumarakom, Kerala, this photo was taken with my Ricoh camera while on a boat ride with my parents. The earthly purple tones, I believe, really stand out in large prints."
         alt="A tranquil evening in Kumarakom, Kerala"
-        url="https://swamiphoto.pixieset.com/landscapes/"
+        url="https://store.swamiphoto.com/wallprints/"
       />
     </main>
   );
