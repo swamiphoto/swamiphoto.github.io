1a978666f9976242ee0d1fed08375e683b1f9791
Swami Venkataramani <swami108@gmail.com>
2024-07-20 18:37:43 -0700

    more mobile fix

--- src/pages/portraits/galleries/Naga.js
+++ src/pages/portraits/galleries/Naga.js
@@ -11,31 +11,36 @@ const Naga = () => {
   };
 
   const captions = {
+    2: "Hello Naga! Make sure to watch till the end for a special message. 🎉",
     4: "Btw...do you have your sound on? 🎶",
     6: "View in fullscreen for the best experience!",
   };
 
   useEffect(() => {
     const fetchImages = async () => {
-      const urls = await fetchImageUrls("portraits/naga-sunflowers");
-      setImageUrls(urls);
-      const imageLoadPromises = urls.map((url) => {
-        return new Promise((resolve) => {
-          const img = new Image();
-          img.onload = resolve;
-          img.onerror = resolve;
-          img.src = url;
+      try {
+        const urls = await fetchImageUrls("portraits/naga-sunflowers");
+        setImageUrls(urls);
+        const imageLoadPromises = urls.map((url) => {
+          return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => resolve(url);
+            img.onerror = () => reject(url);
+            img.src = url;
+          });
         });
-      });
-      await Promise.all(imageLoadPromises);
-      setImagesLoaded(true);
+        await Promise.allSettled(imageLoadPromises);
+        setImagesLoaded(true);
+      } catch (error) {
+        console.error("Error loading images:", error);
+      }
     };
 
     fetchImages();
   }, []);
 
   if (!imagesLoaded) {
-    return <div className="flex items-center justify-center w-full h-screen bg-black text-gray-300 text-2xl font-geist-mono">Preparing your show...please turn your sound on!</div>;
+    return <div className="flex items-center justify-center w-full h-screen  text-gray-800 text-2xl font-geist-mono">Preparing your show...please turn your sound on!</div>;
   }
 
   return (
