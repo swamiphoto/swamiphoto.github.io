447b6bf25f9db8af5abc62df3274095142ce5a57
Swami Venkataramani <swami108@gmail.com>
2024-07-20 18:04:47 -0700

    mobiel debugging

--- src/components/image-gallery/ImageGallery.js
+++ src/components/image-gallery/ImageGallery.js
@@ -21,9 +21,9 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
   const [direction, setDirection] = useState("left");
   const [imagesLoaded, setImagesLoaded] = useState(false);
   const [audioPlaying, setAudioPlaying] = useState(false);
-  const [slideshowPlaying, setSlideshowPlaying] = useState(false); // Initially set to false
+  const [slideshowPlaying, setSlideshowPlaying] = useState(false);
   const [showCover, setShowCover] = useState(true);
-  const [viewMode, setViewMode] = useState("slideshow"); // Added state for view mode
+  const [viewMode, setViewMode] = useState("slideshow");
   const playerRef = useYouTubePlayer(youtubeUrl.split("v=")[1] || youtubeUrl.split("/").pop().split("?")[0]);
   const slideshowInterval = useRef(null);
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
@@ -31,11 +31,11 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
 
   useEffect(() => {
     if (imageUrls.length > 0) {
-      const newTilts = imageUrls.map(() => Math.random() * 12 - 6); // Precompute tilts for each image
-      const newZTilts = imageUrls.map(() => Math.random() * 20 - 10); // Precompute z-tilts for each image
-      const newMoveXs = imageUrls.map(() => `${Math.random() * 15 - 5}px`); // Precompute x-axis movements for each image
-      const newMoveYs = imageUrls.map(() => `${Math.random() * 20 - 5}px`); // Precompute y-axis movements for each image
-      const newDurations = imageUrls.map(() => `${Math.random() * 2 + 3}s`); // Precompute durations for each image
+      const newTilts = imageUrls.map(() => Math.random() * 12 - 6);
+      const newZTilts = imageUrls.map(() => Math.random() * 20 - 10);
+      const newMoveXs = imageUrls.map(() => `${Math.random() * 15 - 5}px`);
+      const newMoveYs = imageUrls.map(() => `${Math.random() * 20 - 5}px`);
+      const newDurations = imageUrls.map(() => `${Math.random() * 2 + 3}s`);
 
       setTilts(newTilts);
       setZTilts(newZTilts);
@@ -43,7 +43,6 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
       setMoveYs(newMoveYs);
       setDurations(newDurations);
 
-      // Fetch image dimensions to determine aspect ratios
       Promise.all(
         imageUrls.map((url) => {
           return new Promise((resolve) => {
@@ -52,7 +51,7 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
               resolve(img.width / img.height);
             };
             img.onerror = () => {
-              resolve(1); // Default aspect ratio if image fails to load
+              resolve(1);
             };
             img.src = url;
           });
@@ -66,7 +65,6 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
 
   useEffect(() => {
     if (layout === "slideshow" && imagesLoaded && imageUrls.length > 0 && slideshowPlaying) {
-      // Start the slideshow with the current image's custom duration
       startSlideshow();
       return () => clearInterval(slideshowInterval.current);
     }
@@ -74,23 +72,23 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
 
   useEffect(() => {
     if (slideshowPlaying) {
-      startSlideshow(); // Start the slideshow initially
-      return () => clearInterval(slideshowInterval.current); // Clean up the interval on unmount or when slideshowPlaying changes
+      startSlideshow();
+      return () => clearInterval(slideshowInterval.current);
     }
   }, [slideshowPlaying]);
 
   const startSlideshow = () => {
     clearInterval(slideshowInterval.current);
-    const duration = customDurations[currentImageIndex] || 4000; // Use custom duration if available, otherwise default to 4 seconds
+    const duration = customDurations[currentImageIndex] || 4000;
     slideshowInterval.current = setTimeout(() => {
       setTransitioning(true);
       setDirection(Math.random() > 0.5 ? "left" : "right");
       setTimeout(() => {
         setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
         setTransitioning(false);
-        startSlideshow(); // Restart the slideshow with the new image's duration
-      }, 2000); // Animation duration
-    }, duration - 2000); // Display duration minus animation duration
+        startSlideshow();
+      }, 2000);
+    }, duration - 2000);
   };
 
   const handlePlayPauseAudio = () => {
@@ -120,7 +118,7 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
   const handleStartClick = () => {
     setShowCover(false);
     handlePlayPauseAudio();
-    setSlideshowPlaying(true); // Start slideshow when the start button is clicked
+    setSlideshowPlaying(true);
   };
 
   const handleToggleFullscreen = () => {
@@ -128,26 +126,20 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
       if (document.documentElement.requestFullscreen) {
         document.documentElement.requestFullscreen();
       } else if (document.documentElement.mozRequestFullScreen) {
-        // Firefox
         document.documentElement.mozRequestFullScreen();
       } else if (document.documentElement.webkitRequestFullscreen) {
-        // Chrome, Safari and Opera
         document.documentElement.webkitRequestFullscreen();
       } else if (document.documentElement.msRequestFullscreen) {
-        // IE/Edge
         document.documentElement.msRequestFullscreen();
       }
     } else {
       if (document.exitFullscreen) {
         document.exitFullscreen();
       } else if (document.mozCancelFullScreen) {
-        // Firefox
         document.mozCancelFullScreen();
       } else if (document.webkitExitFullscreen) {
-        // Chrome, Safari and Opera
         document.webkitExitFullscreen();
       } else if (document.msExitFullscreen) {
-        // IE/Edge
         document.msExitFullscreen();
       }
     }
@@ -155,14 +147,14 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
   };
 
   const handleGridView = () => {
-    setViewMode("grid"); // Switch to grid view
-    clearInterval(slideshowInterval.current); // Stop slideshow if it is running
-    setSlideshowPlaying(false); // Ensure slideshow state is off
+    setViewMode("grid");
+    clearInterval(slideshowInterval.current);
+    setSlideshowPlaying(false);
   };
 
   const renderPhotos = () => {
     if (!imagesLoaded) {
-      return <div>Loading...</div>; // Display a loading message or spinner
+      return <div>Loading...</div>;
     }
 
     if (viewMode === "grid") {
@@ -201,7 +193,7 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
                     backgroundSize: "cover",
                     boxShadow: "2px 2px 8px rgba(0,0,0,0.2)",
                     clipPath: "url(#torn-edge-clip)",
-                    padding: "20px", // Increased padding
+                    padding: "20px",
                   }}>
                   {captions[index]}
                 </div>
@@ -226,7 +218,7 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
             <div className="flex items-center justify-center w-full h-full bg-black text-gray-300 text-2xl font-geist-mono">Preparing your show...please turn your sound on!</div>
           ) : (
             <>
-              <div className="absolute inset-0 w-full h-full bg-black z-10"></div> {/* Fullscreen black layer */}
+              <div className="absolute inset-0 w-full h-full bg-black z-10"></div>
               <img src={imageUrls[isMobile ? mobileCoverImageIndex : coverImageIndex]} alt="" className="absolute inset-0 w-full h-full object-cover z-20 fade-in" />
               <div className="overlay absolute inset-0 bg-black opacity-60 z-30"></div>
               <div className="text-center text-white p-4 z-40 fade-in">
@@ -242,14 +234,14 @@ const ImageGallery = ({ imageUrls, layout = "default", title = "Gallery Title", 
       )}
 
       <div className="hidden md:flex flex-col justify-between items-center w-16 border-r border-gray-300 text-gray-800 p-2 shadow-sm">
-        <div className="flex flex-col items-center text-gray-700 ">
+        <div className="flex flex-col items-center text-gray-700">
           {slideshowPlaying ? (
             <HiOutlinePause className="hover:text-red-500 mt-4 cursor-pointer" size={24} onClick={handlePlayPauseSlideshow} style={{ opacity: 1 }} />
           ) : (
             <AiOutlinePlayCircle className="hover:text-red-500 mt-4 cursor-pointer" size={24} onClick={handlePlayPauseSlideshow} style={{ opacity: 1 }} />
           )}
           <IoMusicalNotesOutline className="hover:text-red-500 mt-4 cursor-pointer" size={20} onClick={handlePlayPauseAudio} style={{ opacity: audioPlaying ? 1 : 0.3 }} />
-          <PiGridNineLight className="hover:text-red-500 mt-4 cursor-pointer" size={20} onClick={handleGridView} style={{ opacity: viewMode === "grid" ? 0.3 : 1 }} /> {/* Grid view icon */}
+          <PiGridNineLight className="hover:text-red-500 mt-4 cursor-pointer" size={20} onClick={handleGridView} style={{ opacity: viewMode === "grid" ? 0.3 : 1 }} />
           {isFullscreen ? (
             <RxExitFullScreen className="hover:text-red-500 mt-4 cursor-pointer" size={20} onClick={handleToggleFullscreen} style={{ opacity: 1 }} />
           ) : (
