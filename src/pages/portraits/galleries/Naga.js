eac60c4fd44360216aff6c2ec22e108b90a8f6d8
Swami Venkataramani <swami108@gmail.com>
2024-07-20 23:35:01 -0700

    random youtube links

--- src/pages/portraits/galleries/Naga.js
+++ src/pages/portraits/galleries/Naga.js
@@ -5,50 +5,57 @@ import { fetchImageUrls } from "../../../common/images";
 const Naga = () => {
   const [imageUrls, setImageUrls] = useState([]);
   const [imagesLoaded, setImagesLoaded] = useState(false);
-  const [minDisplayTimeElapsed, setMinDisplayTimeElapsed] = useState(false);
+  const [randomYouTubeLink, setRandomYouTubeLink] = useState("");
 
-  const customDurations = {
-    2: 6000,
-  };
+  const customDurations = {};
 
   const captions = {
-    4: "Do you have your sound on? 🎶",
-    6: "View in fullscreen for the best experience!",
+    7: "Btw...do you have your sound on? 🎶",
+    9: "View in fullscreen for the best experience!",
+  };
+
+  const youtubeLinks = [
+    "https://www.youtube.com/watch?v=-XTAK0avUEw",
+    "https://www.youtube.com/watch?v=ITswHbJPHhQ",
+    "https://www.youtube.com/watch?v=jx92a0f_gnA",
+    "https://www.youtube.com/watch?v=3yJBhahjQ20",
+    "https://www.youtube.com/watch?v=sc7HiznLyoU",
+    "https://www.youtube.com/watch?v=sc7HiznLyoU",
+    "https://www.youtube.com/watch?v=sc7HiznLyoU",
+    "https://www.youtube.com/watch?v=HzjE33U_gy8",
+    "https://www.youtube.com/watch?v=tyBQ_EHEpqI",
+  ];
+
+  // Function to get a random YouTube link
+  const getRandomYouTubeLink = () => {
+    const randomIndex = Math.floor(Math.random() * youtubeLinks.length);
+    return youtubeLinks[randomIndex];
   };
 
   useEffect(() => {
+    setRandomYouTubeLink(getRandomYouTubeLink());
+
     const fetchImages = async () => {
-      try {
-        const urls = await fetchImageUrls("portraits/naga-sunflowers");
-        setImageUrls(urls);
-        const imageLoadPromises = urls.map((url) => {
-          return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.onload = () => resolve(url);
-            img.onerror = () => reject(url);
-            img.src = url;
-          });
+      const urls = await fetchImageUrls("portraits/naga-sunflowers");
+      setImageUrls(urls);
+      const imageLoadPromises = urls.map((url) => {
+        return new Promise((resolve) => {
+          const img = new Image();
+          img.onload = resolve;
+          img.onerror = resolve;
+          img.src = url;
         });
-        await Promise.allSettled(imageLoadPromises);
-        setImagesLoaded(true);
-      } catch (error) {
-        console.error("Error loading images:", error);
-      }
+      });
+      await Promise.all(imageLoadPromises);
+      setImagesLoaded(true);
     };
 
     fetchImages();
-
-    // Set a timeout to ensure the cover page is displayed for at least 7 seconds
-    const timer = setTimeout(() => {
-      setMinDisplayTimeElapsed(true);
-    }, 5000);
-
-    return () => clearTimeout(timer); // Clean up the timer on unmount
   }, []);
 
-  if (!imagesLoaded || !minDisplayTimeElapsed) {
+  if (!imagesLoaded) {
     return (
-      <div className="flex flex-col items-center justify-center w-full h-screen bg-black text-gray-200 md:text-2xl font-geist-mono overflow-hidden m-0 p-0">
+      <div className="flex flex-col items-center justify-center w-full h-screen bg-black text-gray-200 text-2xl font-geist-mono overflow-hidden m-0 p-0">
         <div>Preparing your show...please turn your sound on!</div>
         <div className="text-sm text-gray-400 mt-2">Designed and conceptualized by Swami Venkataramani</div>
       </div>
@@ -57,18 +64,7 @@ const Naga = () => {
 
   return (
     <div className="bg-gray-200">
-      <ImageGallery
-        imageUrls={imageUrls}
-        layout="slideshow"
-        title="Sunflower Soundarya"
-        subtitle="A dreamy evening with the sunflowers in Woodland."
-        youtubeUrl="https://www.youtube.com/watch?v=-XTAK0avUEw"
-        customDurations={customDurations}
-        captions={captions}
-        coverImageIndex={4}
-        mobileCoverImageIndex={3}
-        hideCaptionsOnMobile={true} // Pass the prop to hide captions on mobile
-      />
+      <ImageGallery imageUrls={imageUrls} layout="slideshow" title="Sunflower Soundarya" subtitle="A dreamy evening with the sunflowers in Woodland." youtubeUrl={randomYouTubeLink} customDurations={customDurations} captions={captions} coverImageIndex={4} mobileCoverImageIndex={4} />
     </div>
   );
 };
