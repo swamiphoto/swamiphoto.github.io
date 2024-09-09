e0c8208a93acfd19ce031aee3bb081419917062f
Swami Venkataramani <swami108@gmail.com>
2024-09-09 12:32:17 -0700

    gallery change

--- src/components/image-displays/slideshow/Loading/Loading.js
+++ src/components/image-displays/slideshow/Loading/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => (
   <div className="flex flex-col items-center justify-center w-full h-screen text-gray-800 md:text-2xl font-geist-mono overflow-hidden m-0 p-0">
     <div>Loading your show...</div>
-    <div className="text-sm text-gray-500 mt-2">Design and concept by Swami Venkataramani</div>
+    <div className="text-sm text-gray-500 mt-2">Designed by Swami Venkataramani</div>
   </div>
 );
 
