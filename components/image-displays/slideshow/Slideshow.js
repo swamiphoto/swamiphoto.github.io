637e952734746894b3aee4da9629d1729ab356ee
Swami Venkataramani <swami108@gmail.com>
2024-09-19 23:16:35 -0700

    modal css

--- components/image-displays/slideshow/Slideshow.js
+++ components/image-displays/slideshow/Slideshow.js
@@ -200,8 +200,8 @@ const Slideshow = ({ imageUrls, layout = "film-stack", title = "Gallery Title", 
           {/* Translucent white background */}
           <div className="absolute inset-0 bg-gray-300 opacity-95"></div>
 
-          {/* Modal with min and max height */}
-          <div className="relative z-50 bg-white rounded-2xl shadow-xl w-full max-w-lg mx-auto flex flex-col" style={{ margin: "15px", minHeight: "calc(100vh - 30px)", maxHeight: "calc(100vh - 30px)" }}>
+          {/* Modal with min and max dynamic height */}
+          <div className="relative z-50 bg-white rounded-2xl shadow-xl w-full max-w-lg mx-auto flex flex-col" style={{ margin: "15px", minHeight: "calc(100dvh - 30px)", maxHeight: "calc(100dvh - 30px)" }}>
             {/* Close button positioned over the image in top-right corner */}
             <button onClick={() => router.push(`/galleries/${slug}`)} className="absolute top-5 right-5 z-60 text-gray-500 hover:text-gray-800">
               <TfiClose className="h-5 w-5" />
@@ -223,7 +223,7 @@ const Slideshow = ({ imageUrls, layout = "film-stack", title = "Gallery Title", 
               <button
                 onClick={() => {
                   setIsModalOpen(false);
-                  handlePlayPauseSlideshow();
+                  handlePlayPauseSlideshow(); // Start slideshow
                 }}
                 className="w-full px-8 py-4 bg-black text-white font-bold uppercase tracking-wider cursor-pointer">
                 Start Slideshow
