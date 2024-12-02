1ac205f189c1fb60540cb52a4d9f6fd4942c52db
Swami Venkataramani <swami108@gmail.com>
2024-12-02 09:50:14 -0800

    copy

--- pages/timemanagement.js
+++ pages/timemanagement.js
@@ -62,14 +62,19 @@ const TimeManagement = () => {
           <h2 className="text-2xl font-medium text-gray-700 italic mt-4">Achieve in 13 weeks what most people struggle to accomplish in a year.</h2>
           <WiggleLine />
           <p className="mt-4">
-            Most people spend 8 hours working but only get 3 hours of actual work done. Why not work only 3 hours and use the extra 5 hours for whatever else? And here’s another sobering fact—only 6% of people achieve meaningful goals each year. The rest of us are stuck in a cycle of busyness,
-            juggling tasks that don’t move the needle.
+            <span className="bg-yellow-200">Most people spend 8 hours working but only get 3 hours of actual work done.</span> Why not work only 3 hours and use the extra 5 hours for whatever else? And here’s another sobering fact—
+            <span className="bg-yellow-200">only 6% of people achieve meaningful goals each year.</span> The rest of us are always busy, juggling tasks that don’t move the needle.
           </p>
-          <p className="mt-4">Ready to break free from old patterns? I'll teach you how to manage your time, and accomplish in 13 weeks what others take a year to achieve.</p>
+          <p className="mt-4">
+            Ready to break free from old patterns? <span className="bg-yellow-200">I'll teach you how to manage your time</span>, and accomplish in 13 weeks what others take a year to achieve.
+          </p>
+
           <form ref={formRef} onSubmit={handleSignUp} className="space-y-4 mt-8">
             <div>
-              <span className="text-2xl font-bold">Sign up now and save 75%!</span>
-              <span className="ml-2 text-2xl line-through">$487</span>
+              <span className="text-2xl font-bold">
+                Sign up now and <span className="bg-yellow-200">save 85%</span>
+              </span>
+              <span className="ml-2 text-2xl line-through">$299</span>
               <span className="ml-2 text-2xl font-bold">$47</span>
             </div>
             <div>
