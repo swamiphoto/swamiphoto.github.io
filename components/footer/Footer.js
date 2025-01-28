afd319292ec2b12b9317b70431cf070f6a3c328f
Swami Venkataramani <swami108@gmail.com>
2025-01-28 10:56:01 -0800

    footer change

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,12 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="mx-auto p-8 md:p-24 pt-0 mt-10 text-gray-700">
-      <div>All images copyright © 2025 Swami Venkataramani</div>
-      <div className="text-sm mt-1 ">
-        Website is{" "}
+      <div className="text-sm md:text-base">All images copyright © 2025 Swami Venkataramani</div>
+      <div className="text-sm mt-1">
+        Website{" "}
         <b>
-          <a href="https://svenkastudio.com" target="_blank" className="underline hover:text-red-500">
-            custom-designed
+          <a href="https://svenkastudio.com" target="_blank" className="underline hover:text-red-500 font-normal">
+            designed and built
           </a>
         </b>{" "}
         by me.{" "}
